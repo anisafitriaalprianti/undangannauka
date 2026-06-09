28b03d84b802f04ac5ad1def4ef68faca8034bb4
anisafitriaalprianti <annisafitriaalprianti@gmail.com>
2026-06-10 05:57:02 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-  /* config options here */
+  reactStrictMode: true,
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: false,
   },
-  reactStrictMode: false,
 };
 
 export default nextConfig;
